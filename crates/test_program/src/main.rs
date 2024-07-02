e6ea071127d0ed95e528a7f3b98a61cc33fd36a7
Lander Brandt <landerbrandt@gmail.com>
2024-07-01 22:54:45 -0700

    test_program: drop file to disk if compiled without debug feature

--- crates/test_program/src/main.rs
+++ crates/test_program/src/main.rs
@@ -1,11 +1,25 @@
+#[cfg(feature = "debug")]
 use log::*;
 
 fn main() {
-    log::set_logger(&win_dbg_logger::DEBUGGER_LOGGER).expect("failed to set logger");
-    log::set_max_level(log::LevelFilter::Debug);
+    #[cfg(feature = "debug")]
+    {
+        log::set_logger(&win_dbg_logger::DEBUGGER_LOGGER).expect("failed to set logger");
+        log::set_max_level(log::LevelFilter::Debug);
 
-    debug!(
-        "Hello from a full exe! My main function is located at 0x{:#x}",
-        main as usize
-    );
+        debug!(
+            "Hello from a full exe! My main function is located at 0x{:#x}",
+            main as usize
+        );
+    }
+
+    #[cfg(not(feature = "debug"))]
+    {
+        let app_data = std::env::var("LOCALAPPDATA").expect("failed to get %LOCALAPPDATA%");
+        std::fs::write(
+            format!("{}\\..\\LocalState\\stage3_complete.txt", app_data),
+            b"if you're reading this, stage3 successfully loaded and ran by the PE loader",
+        )
+        .expect("failed to write to LocalState");
+    }
 }
