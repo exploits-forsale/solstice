0e4185ee519bc43005160a94ac55d511a6ed26a4
Lander Brandt <landerbrandt@gmail.com>
2024-07-08 19:48:54 -0700

    test_program_rust: dump command line arguments to file

--- crates/test_program_rust/src/main.rs
+++ crates/test_program_rust/src/main.rs
@@ -1,6 +1,8 @@
 #[cfg(feature = "debug")]
 use log::*;
 
+use std::io::Write;
+
 fn main() {
     #[cfg(feature = "debug")]
     {
@@ -21,10 +23,17 @@ fn main() {
     #[cfg(not(feature = "debug"))]
     {
         let app_data = std::env::var("LOCALAPPDATA").expect("failed to get %LOCALAPPDATA%");
-        std::fs::write(
-            format!("{}\\..\\LocalState\\stage3_complete.txt", app_data),
-            b"if you're reading this, stage3 successfully loaded and ran by the PE loader",
+        let mut file =
+            std::fs::File::create(format!("{}\\..\\LocalState\\stage3_complete.txt", app_data))
+                .unwrap();
+        writeln!(
+            &mut file,
+            "if you're reading this, stage3 successfully loaded and ran by the PE loader"
         )
-        .expect("failed to write to LocalState");
+        .expect("failed to write to output");
+
+        for arg in std::env::args() {
+            writeln!(&mut file, "{:p} {}", arg.as_ptr(), arg).expect("failed to write to outpu");
+        }
     }
 }
